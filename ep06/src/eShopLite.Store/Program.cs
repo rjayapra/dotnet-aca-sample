40b0d26f75b41b4685fd8eaaa4eb2ff36aacbd9f
Frank <fboucher@outlook.com>
2025-01-09 17:49:21 -0500

    adding source code for the apps

---
+++ ep06/src/eShopLite.Store/Program.cs
@@ -0,0 +1,53 @@
+using eShopLite.Store.ApiClients;
+using eShopLite.Store.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+// Add razor components
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+// Add HTTP clients
+builder.Services.AddHttpClient<ProductApiClient>(client =>
+{
+    var productsApiUrl = builder.Configuration.GetValue<string>("ProductsApi");
+    if (string.IsNullOrEmpty(productsApiUrl))
+    {
+        throw new ArgumentNullException(nameof(productsApiUrl), "ProductsApi configuration value is missing or empty.");
+    }
+    client.BaseAddress = new Uri(productsApiUrl);
+});
+
+builder.Services.AddHttpClient<WeatherApiClient>(client =>
+{
+    var weatherApiUrl = builder.Configuration.GetValue<string>("WeatherApi");
+    if (string.IsNullOrEmpty(weatherApiUrl))
+    {
+        throw new ArgumentNullException(nameof(weatherApiUrl), "WeatherApi configuration value is missing or empty.");
+    }
+    client.BaseAddress = new Uri(weatherApiUrl);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
+
+app.Run();
